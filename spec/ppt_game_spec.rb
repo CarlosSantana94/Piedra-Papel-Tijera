3311962dff32af4c48ca70b47aff584121bae560
carlos santana p <cgsp94@gmail.com>
2020-11-27 15:49:23 +0000

    creacion de metodo de seleccion de maquina, refactor de metodo de seleccion usuario

--- spec/ppt_game_spec.rb
+++ spec/ppt_game_spec.rb
@@ -1,9 +1,16 @@
 require './lib/ppt_game.rb'
 
 describe PiedraPapelTijera do
- it '' do
-    ppt= PiedraPapelTijera.new 
-    seleccion= ppt.seleccion("Piedra")
-    expect(seleccion).to eq "Piedra"
- end   
+    it 'Muestra la seleccion del usuario' do
+        ppt= PiedraPapelTijera.new 
+        seleccion= ppt.seleccion_usuario("Piedra")
+        expect(seleccion).to eq "Piedra"
+    end   
+
+    it 'Muestra la seleccion de la maquina se envia 1 y regresa Papel' do
+        ppt= PiedraPapelTijera.new 
+        seleccion= ppt.seleccion_maquina(1)
+        expect(seleccion).to eq "Papel"
+    end
+
 end
