5671faa5f62e6ea54f453fb762afb29332d8f5fd
carlos santana p <cgsp94@gmail.com>
2020-11-27 16:02:37 +0000

    refactor general

--- app.rb
+++ app.rb
@@ -5,12 +5,16 @@ require './lib/ppt_game.rb'
 enable :sessions
 
 get '/' do
+    session[:feedback] = ""
+    session[:maquina] = ""
     erb :index
 end
 
 post '/jugar' do
-    # ppt= PiedraPapelTijera.new 
-    # seleccion= ppt.seleccion(params[:seleccion]))
+    ppt= PiedraPapelTijera.new 
+    seleccion_usuario = ppt.seleccion_usuario(params[:seleccion])
+    seleccion_maquina = ppt.seleccion_maquina(1)
     session[:feedback] = "Tu selección fue: #{params[:seleccion]}"
+    session[:maquina] = "La computadora seleccionó: #{seleccion_maquina}"
     erb :index
 end
